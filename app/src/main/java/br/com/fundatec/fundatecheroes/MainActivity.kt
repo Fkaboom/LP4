e3cb047704de0af6b1c647f41d73d70309f1c4e6
Fkaboom <f.kaboom5@gmail.com>
2023-04-04 15:30:54 -0300

    atividade modulo

--- app/src/main/java/br/com/fundatec/fundatecheroes/MainActivity.kt
+++ app/src/main/java/br/com/fundatec/fundatecheroes/MainActivity.kt
@@ -2,15 +2,10 @@ package br.com.fundatec.fundatecheroes
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("StringFormatInvalid")
@@ -24,4 +19,5 @@ class MainActivity : AppCompatActivity() {
             finish()
         }, 3000)
     }
+
     }
